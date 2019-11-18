ec2c07cee0d95ffb847886aedf5b3af809fd0543
Nnamdi Iroham <niroham.yahoo.com>
2019-11-18 11:50:12 -0800

    Updates

---
+++ precache-manifest.4c99711a5f06525f17688b694fa66aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ceb5e40737588f643b5c7425efd3ec",
+    "url": "/click-click/index.html"
+  },
+  {
+    "revision": "7ff44367122d4c7aa756",
+    "url": "/click-click/static/css/main.0ae19c37.chunk.css"
+  },
+  {
+    "revision": "44fd5329b84e54436449",
+    "url": "/click-click/static/js/2.a5bbeccd.chunk.js"
+  },
+  {
+    "revision": "7ff44367122d4c7aa756",
+    "url": "/click-click/static/js/main.adf26c70.chunk.js"
+  },
+  {
+    "revision": "4687ffec596d73a13d5b",
+    "url": "/click-click/static/js/runtime-main.28971a75.js"
+  }
+]);
